4d4ea4cf9216f635fb0efe8be1955e1278d56b85
darvasd <darvas.daniel@gmail.com>
2013-04-06 16:43:04 +0200

    ANT build batch file added

---
+++ build.bat
@@ -0,0 +1,6 @@
+SET JAVA_HOME=c:\Program Files\Java\jdk1.7.0\
+SET ANT_LOCATION=d:\Programok\EclipseJuno\plugins\org.apache.ant_1.8.3.v20120321-1730\bin\
+
+@cd hu.documaison.gui
+%ANT_LOCATION%\ant build-project
+@cd ..
