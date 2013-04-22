3a20b7206b91e37a0ef686b4009debaaa5e3468a
darvasd <darvas.daniel@gmail.com>
2013-04-22 20:52:42 +0200

    Ant updated.

--- clean.bat
+++ clean.bat
@@ -2,5 +2,5 @@ SET JAVA_HOME=c:\Program Files\Java\jdk1.7.0\
 SET ANT_LOCATION=d:\Programok\EclipseJuno\plugins\org.apache.ant_1.8.3.v20120321-1730\bin\
 
 @cd hu.documaison.gui
-start /B /WAIT %ANT_LOCATION%\ant build
+start /B /WAIT %ANT_LOCATION%\ant cleanall
 @cd ..
--- run.bat
+++ run.bat
@@ -2,5 +2,6 @@ SET JAVA_HOME=c:\Program Files\Java\jdk1.7.0\
 SET ANT_LOCATION=d:\Programok\EclipseJuno\plugins\org.apache.ant_1.8.3.v20120321-1730\bin\
 
 @cd hu.documaison.gui
-start /B /WAIT %ANT_LOCATION%\ant build
+@REM copy ..\hu.documaison.dal\src\log4j.xml bin\hu\documaison\gui\log4j.xml
+%ANT_LOCATION%\ant DocuMaisonWindow
 @cd ..
